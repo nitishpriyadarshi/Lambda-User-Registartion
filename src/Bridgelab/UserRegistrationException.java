8a4210ca68157ff2b7593fd34a95007501d71b39
nitishpriyadarshi <nitish8152@gmail.com>
2022-09-05 15:41:14 +0530

    Welcome

---
+++ src/Bridgelab/UserRegistrationException.java
@@ -0,0 +1,13 @@
+package Bridgelab;
+
+public class UserRegistrationException extends Exception  {
+    public ExceptionType type;
+    public enum ExceptionType {
+        INVALID_FIRST_NAME, INVALID_LAST_NAME, INVALID_MOBILE_NUMBER, INVALID_EMAIL_ID, INVALID_PASSWORD,
+    }
+
+    public UserRegistrationException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+}
